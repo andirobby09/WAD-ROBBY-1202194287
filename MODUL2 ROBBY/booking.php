b1d74ad0d0fc3739c776026295ff4947ab96a0e5
krisnadp <krisna10permana@gmail.com>
2021-10-25 08:07:29 +0700

    m2

--- MODUL2 ROBBY/booking.php
+++ MODUL2 ROBBY/booking.php
@@ -1,135 +1,135 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <title>Document</title>
-</head>
-<body>
-    <!--Navbar-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container justify-content-center">
-        <div id="navbarNav">
-          <ul class="nav navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="booking.php">Booking</a>
-            </li>
-          </ul>
+      <link rel="stylesheet" href="style.css">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+      <title>Document</title>
+  </head>
+  <body>
+      <!--Navbar-->
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container justify-content-center">
+          <div id="navbarNav">
+            <ul class="nav navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="booking.php">Booking</a>
+              </li>
+            </ul>
+          </div>
         </div>
+      </nav>
+      <!--Heading-->
+      <div class="container heading text-center">
+          <div class="container bg-dark" id="text">Reserve your venue now!</div>
       </div>
-    </nav>
-    <!--Heading-->
-    <div class="container heading text-center">
-        <div class="container bg-dark" id="text">Reserve your venue now!</div>
-    </div>
-    <!-- Get and Post -->
-    <div class="container">
-        <div class="container">
-            <div class="row border p-2">
-                <div class="col align-self-center" for="tes">
-                    <?php if (isset($_POST['img'])): ?>
-                      <img src="<?= $_POST['nama']?>" class="rounded mx-auto d-block" style="width: 30rem;" alt="foto">
-                    <?php elseif (isset($_POST['img2'])): ?>
-                      <img src="<?= $_POST['nama']?>" class="rounded mx-auto d-block" style="width: 30rem;" alt="foto">
-                    <?php elseif (isset($_POST['img3'])): ?>
-                      <img src="<?= $_POST['nama']?>" class="rounded mx-auto d-block" style="width: 30rem;" alt="foto">
-                    <?php else: ?>
-                      <img src="nusantarahall.jpeg" class="rounded mx-auto d-block" style="width: 30rem;">
-                    <?php endif;?>
-                </div>
+      <!-- Get and Post -->
+      <div class="container">
+          <div class="container">
+              <div class="row border p-2">
+                  <div class="col align-self-center" for="tes">
+                      <?php if (isset($_POST['img'])): ?>
+                        <img src="<?= $_POST['nama']?>" class="rounded mx-auto d-block" style="width: 30rem;" alt="foto">
+                      <?php elseif (isset($_POST['img2'])): ?>
+                        <img src="<?= $_POST['nama']?>" class="rounded mx-auto d-block" style="width: 30rem;" alt="foto">
+                      <?php elseif (isset($_POST['img3'])): ?>
+                        <img src="<?= $_POST['nama']?>" class="rounded mx-auto d-block" style="width: 30rem;" alt="foto">
+                      <?php else: ?>
+                        <img src="nusantarahall.jpeg" class="rounded mx-auto d-block" style="width: 30rem;">
+                      <?php endif;?>
+                  </div>
 
-                <div class="col">
-                    <form action="mybooking.php" method="POST">
-                      <!-- nama -->
-                      <div class="mb-3">
-                          <label for="inputnama" class="form-label">Name</label>
-                          <input class="form-control" name="nama" type="text" value="robby_1202194287" disabled readonly id="inputnama">
-                      </div>
-                      <!-- tanggal -->
-                      <div class="mb-3">
-                          <label for="inputtanggal" class="form-label">Event Date</label>
-                          <input type="date" class="form-control" id="inputtanggal" name="tgl" placeholder="mm/dd/yyyy">
-                      </div>
-                      <!-- waktu -->
-                      <div class="mb-3">
-                          <label for="inputwaktu" class="form-label">Start Time</label>
-                          <input type="text" class="form-control" id="inputwaktu" name="waktu" placeholder="--:--:--">
-                      </div>
-                      <!-- durasi -->
-                      <div class="mb-3">
-                          <label for="inputdurasi" class="form-label">Duration (Hours)</label>
-                          <input type="number" class="form-control" id="inputdurasi">
-                      </div>
-                      <!-- typegedung -->
-                      <div class="mb-3">
-                          <label for="inputtypegedung" class="form-label">Building Type</label>
-                          <select class="form-select form-select-sm" name="tipe" aria-label=".form-select-sm example">
-                          
-                            <?php if (isset($_POST['img3'])): ?>
-                              <option value="3">Choose..</option>
-                              <option value="Nusantara Hall">Nusantara Hall</option>
-                              <option value="Garuda Hall">Garuda Hall</option>
-                              <option selected>Gedung Serba Guna  
-                              </option>
-                            <?php elseif (isset($_POST['img2'])): ?>
-                              <option value="2">Choose..</option>
-                              <option value="Nusantara Hall">Nusantara Hall</option>
-                              <option selected>Garuda Hall</option>
-                              <option value="3">Gedung Serba Guna</option>
-                            <?php elseif (isset($_POST['img'])): ?>
-                              <option value="1">Choose..</option>
-                              <option selected>Nusantara Hall</option>
-                              <option value="Garuda Hall">Garuda Hall</option>
-                              <option value="3">Gedung Serba Guna</option>
-                            <?php else: ?>
-                              <option selected>Choose..</option>
-                              <option value="Nusantara Hall">Nusantara Hall</option>
-                              <option value="Garuda Hall">Garuda Hall</option>
-                              <option value="Gedung Serba Guna">Gedung Serba Guna</option>
-                            <?php endif;?>
-                          </select>
-                      </div>
-                      <!-- nohp -->
-                      <div class="mb-3">
-                          <label for="inputnohp" class="form-label">Phone Number</label>
-                          <input type="text" class="form-control" name="nohp" id="inputnohp">
-                      </div>
-                      <!-- service -->
-                      <label for="inputnohp" class="form-label">Add Service(s)</label>
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="service" value="Catering" id="flexCheckDefault">
-                        <label class="form-check-label" for="flexCheckDefault">
-                          Catering / $700
-                        </label>
-                      </div>
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="service1"value="Decoration" id="flexCheckChecked">
-                        <label class="form-check-label" for="flexCheckChecked">
-                          Decoration / $450
-                        </label>
-                      </div>
-                      <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="service2" value="Sound System" id="flexCheckChecked">
-                        <label class="form-check-label" for="flexCheckChecked">
-                          Sound System / $250
-                        </label>
-                      </div>
-                      <!-- Button -->
-                      <div class="d-grid gap-2 mt-3">
-                          <button class="btn btn-primary" type="submit" name="book">Book</button>
-                      </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-<footer class="text-center">Created by: Robby_1202194287</footer>
-</html>
+                  <div class="col">
+                      <form action="mybooking.php" method="POST">
+                        <!-- nama -->
+                        <div class="mb-3">
+                            <label for="inputnama" class="form-label">Name</label>
+                            <input class="form-control" name="nama" type="text" value="robby_1202194287" disabled readonly id="inputnama">
+                        </div>
+                        <!-- tanggal -->
+                        <div class="mb-3">
+                            <label for="inputtanggal" class="form-label">Event Date</label>
+                            <input type="date" class="form-control" id="inputtanggal" name="tgl" placeholder="mm/dd/yyyy">
+                        </div>
+                        <!-- waktu -->
+                        <div class="mb-3">
+                            <label for="inputwaktu" class="form-label">Start Time</label>
+                            <input type="text" class="form-control" id="inputwaktu" name="waktu" placeholder="--:--:--">
+                        </div>
+                        <!-- durasi -->
+                        <div class="mb-3">
+                            <label for="inputdurasi" class="form-label">Duration (Hours)</label>
+                            <input type="number" class="form-control" id="inputdurasi">
+                        </div>
+                        <!-- typegedung -->
+                        <div class="mb-3">
+                            <label for="inputtypegedung" class="form-label">Building Type</label>
+                            <select class="form-select form-select-sm" name="tipe" aria-label=".form-select-sm example">
+                            
+                              <?php if (isset($_POST['img3'])): ?>
+                                <option value="3">Choose..</option>
+                                <option value="Nusantara Hall">Nusantara Hall</option>
+                                <option value="Garuda Hall">Garuda Hall</option>
+                                <option selected>Gedung Serba Guna  
+                                </option>
+                              <?php elseif (isset($_POST['img2'])): ?>
+                                <option value="2">Choose..</option>
+                                <option value="Nusantara Hall">Nusantara Hall</option>
+                                <option selected>Garuda Hall</option>
+                                <option value="3">Gedung Serba Guna</option>
+                              <?php elseif (isset($_POST['img'])): ?>
+                                <option value="1">Choose..</option>
+                                <option selected>Nusantara Hall</option>
+                                <option value="Garuda Hall">Garuda Hall</option>
+                                <option value="3">Gedung Serba Guna</option>
+                              <?php else: ?>
+                                <option selected>Choose..</option>
+                                <option value="Nusantara Hall">Nusantara Hall</option>
+                                <option value="Garuda Hall">Garuda Hall</option>
+                                <option value="Gedung Serba Guna">Gedung Serba Guna</option>
+                              <?php endif;?>
+                            </select>
+                        </div>
+                        <!-- nohp -->
+                        <div class="mb-3">
+                            <label for="inputnohp" class="form-label">Phone Number</label>
+                            <input type="text" class="form-control" name="nohp" id="inputnohp">
+                        </div>
+                        <!-- service -->
+                        <label for="inputnohp" class="form-label">Add Service(s)</label>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" name="service" value="Catering" id="flexCheckDefault">
+                          <label class="form-check-label" for="flexCheckDefault">
+                            Catering / $700
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" name="service1"value="Decoration" id="flexCheckChecked">
+                          <label class="form-check-label" for="flexCheckChecked">
+                            Decoration / $450
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" type="checkbox" name="service2" value="Sound System" id="flexCheckChecked">
+                          <label class="form-check-label" for="flexCheckChecked">
+                            Sound System / $250
+                          </label>
+                        </div>
+                        <!-- Button -->
+                        <div class="d-grid gap-2 mt-3">
+                            <button class="btn btn-primary" type="submit" name="book">Book</button>
+                        </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </body>
+  <footer class="text-center">Created by: Robby_1202194287</footer>
+  </html>
--- MODUL2 ROBBY/index.php
+++ MODUL2 ROBBY/index.php
@@ -1,105 +1,105 @@
-<!DOCTYPE htm l>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!DOCTYPE htm l>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <title>Document</title>
-  </head>
-  <body>
-    <!--Navbar-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container justify-content-center">
-        <div id="navbarNav">
-          <ul class="nav navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="booking.php">Booking</a>
-            </li>
-          </ul>
+        <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+        <title>Document</title>
+      </head>
+      <body>
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container justify-content-center">
+            <div id="navbarNav">
+              <ul class="nav navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="booking.php">Booking</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <!--Heading-->
+        <div class="heading text-center">
+          <h5>WELCOME TO ESD VENUE RESERVATION</h5>
+          <div class="container bg-dark" id="text">Find your best deal for your event, here!</div>
         </div>
-      </div>
-    </nav>
-    <!--Heading-->
-    <div class="heading text-center">
-      <h5>WELCOME TO ESD VENUE RESERVATION</h5>
-      <div class="container bg-dark" id="text">Find your best deal for your event, here!</div>
-    </div>
-    <!--Card-->
-    <div class="gedung">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-4">
-            <div class="card" style="width: 22rem;">
-              <img src="nusantarahall.jpeg" class="card-img-top" height="200">
-              <div class="card-body">
-                <h5 class="card-title">Nusantara Hall</h5>
-                <p class="card-text">$2000 / Hour<br>5000 Capacity</p>
+        <!--Card-->
+        <div class="gedung">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-4">
+                <div class="card" style="width: 22rem;">
+                  <img src="nusantarahall.jpeg" class="card-img-top" height="200">
+                  <div class="card-body">
+                    <h5 class="card-title">Nusantara Hall</h5>
+                    <p class="card-text">$2000 / Hour<br>5000 Capacity</p>
+                  </div>
+                  <ul class="list-group list-group-flush text-center">
+                    <li class="list-group-item fw-bold text-success">Free Parking</li>
+                    <li class="list-group-item fw-bold text-success">Full AC</li>
+                    <li class="list-group-item fw-bold text-success">Cleaning Service</li>
+                    <li class="list-group-item fw-bold text-success">Covid-19 Health Protocol</li>
+                  </ul>
+                  <div class="card-body text-center">
+                    <form action="mybooking.php" method="POST">
+                      <input name="img" value="nusantarahall.jpeg" hidden>
+                      <button class="btn btn-primary" type="submit">Book now</button>
+                    </form>
+                  </div>
+                </div>
               </div>
-              <ul class="list-group list-group-flush text-center">
-                <li class="list-group-item fw-bold text-success">Free Parking</li>
-                <li class="list-group-item fw-bold text-success">Full AC</li>
-                <li class="list-group-item fw-bold text-success">Cleaning Service</li>
-                <li class="list-group-item fw-bold text-success">Covid-19 Health Protocol</li>
-              </ul>
-              <div class="card-body text-center">
-                <form action="mybooking.php" method="POST">
-                  <input name="img" value="nusantarahall.jpeg" hidden>
-                  <button class="btn btn-primary" type="submit">Book now</button>
-                </form>
+              <div class="col-sm-4">
+                <div class="card" style="width: 22rem;">
+                  <img src="garudahall.jpg" class="card-img-top" height="200" >
+                  <div class="card-body">
+                    <h5 class="card-title">Garuda Hall</h5>
+                    <p class="card-text">$1000 / Hour<br>2000 Capacity</p>
+                  </div>
+                  <ul class="list-group list-group-flush text-center">
+                    <li class="list-group-item fw-bold text-success">Free Parking</li>
+                    <li class="list-group-item fw-bold text-success">Full AC</li>
+                    <li class="list-group-item fw-bold text-danger">No Cleaning Service</li>
+                    <li class="list-group-item fw-bold text-success">Covid-19 Health Protocol</li>
+                  </ul>
+                  <div class="card-body text-center">
+                    <form action="booking.php" method="POST">
+                      <input name="img2" value="garudahall.jpg" hidden>
+                      <button class="btn btn-primary" type="submit">Book now</button>
+                    </form>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="col-sm-4">
-            <div class="card" style="width: 22rem;">
-              <img src="garudahall.jpg" class="card-img-top" height="200" >
-              <div class="card-body">
-                <h5 class="card-title">Garuda Hall</h5>
-                <p class="card-text">$1000 / Hour<br>2000 Capacity</p>
-              </div>
-              <ul class="list-group list-group-flush text-center">
-                <li class="list-group-item fw-bold text-success">Free Parking</li>
-                <li class="list-group-item fw-bold text-success">Full AC</li>
-                <li class="list-group-item fw-bold text-danger">No Cleaning Service</li>
-                <li class="list-group-item fw-bold text-success">Covid-19 Health Protocol</li>
-              </ul>
-              <div class="card-body text-center">
-                <form action="booking.php" method="POST">
-                  <input name="img2" value="garudahall.jpg" hidden>
-                  <button class="btn btn-primary" type="submit">Book now</button>
-                </form>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-4">
-            <div class="card" style="width: 22rem;">
-              <img src="gedungserbaguna.jpg" class="card-img-top" height="200">
-              <div class="card-body">
-                <h5 class="card-title">Gedung Serba Guna</h5>
-                <p class="card-text">$500 / Hour<br>500 Capacity</p>
-              </div>
-              <ul class="list-group list-group-flush text-center">
-                <li class="list-group-item fw-bold text-danger">No Free Parking</li>
-                <li class="list-group-item fw-bold text-danger">No Full AC</li>
-                <li class="list-group-item fw-bold text-danger">No Cleaning Service</li>
-                <li class="list-group-item fw-bold text-success">Covid-19 Health Protocol</li>
-              </ul>
-              <div class="card-body text-center">
-                <form action="booking.php" method="POST">
-                  <input name="img3" value="gedungserbaguna.jpg" hidden>
-                  <button class="btn btn-primary" type="submit">Book now</button>
-                </form>
+              <div class="col-sm-4">
+                <div class="card" style="width: 22rem;">
+                  <img src="gedungserbaguna.jpg" class="card-img-top" height="200">
+                  <div class="card-body">
+                    <h5 class="card-title">Gedung Serba Guna</h5>
+                    <p class="card-text">$500 / Hour<br>500 Capacity</p>
+                  </div>
+                  <ul class="list-group list-group-flush text-center">
+                    <li class="list-group-item fw-bold text-danger">No Free Parking</li>
+                    <li class="list-group-item fw-bold text-danger">No Full AC</li>
+                    <li class="list-group-item fw-bold text-danger">No Cleaning Service</li>
+                    <li class="list-group-item fw-bold text-success">Covid-19 Health Protocol</li>
+                  </ul>
+                  <div class="card-body text-center">
+                    <form action="booking.php" method="POST">
+                      <input name="img3" value="gedungserbaguna.jpg" hidden>
+                      <button class="btn btn-primary" type="submit">Book now</button>
+                    </form>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </body>
-  <footer class="text-center">Created by: Robby_1202194287</footer>
-</html>
+      </body>
+      <footer class="text-center">Created by: Robby_1202194287</footer>
+    </html>
